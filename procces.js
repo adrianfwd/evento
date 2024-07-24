c8561f5a3694346f7d35c9204715e524fc008091
adrianfwd <azuniga@fwdcostarica.com>
2024-07-24 13:35:08 -0600

    se agregan botones y funcionalidad en 3 paginas diferentes

--- procces.js
+++ procces.js
@@ -1,55 +1,44 @@
-document.getElementById('register-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  const regUsername = document.getElementById('reg-username').value;
-  const regPassword = document.getElementById('reg-password').value;
 
+document.getElementById('register-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  var regUsername = document.getElementById('reg-username').value;
+  var regPassword = document.getElementById('reg-password').value;
 
   if (regUsername && regPassword) {
-      let users = JSON.parse(localStorage.getItem('users')) || [];
-      let userExists = users.some(user => user.username === regUsername);
+    var users = JSON.parse(localStorage.getItem('users')) || [];
+    let userExists = users.some(user => user.username === regUsername);
 
-      if (!userExists) {
-          users.push({ username: regUsername, password: regPassword });
-          localStorage.setItem('users', JSON.stringify(users));
-          alert('Usuario registrado exitosamente.');
-          document.getElementById('register-container').style.display = 'none';
-          document.getElementById('login-container').style.display = 'block';
-      } else {
-          alert('El usuario ya existe.');
-      }
+    if (!userExists) {
+      users.push({ username: regUsername, password: regPassword });
+      localStorage.setItem('users', JSON.stringify(users));
+      alert('Usuario registrado exitosamente.');
+    } else {
+      alert('El usuario ya existe.');
+    }
   }
 });
 
-document.getElementById('login-form').addEventListener('submit', function(event) {
+
+
+
+//------------------------------------------------------------------------------------|
+document.getElementById('login-form').addEventListener('submit', function (event) {
   event.preventDefault();
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+  var regUsername = document.getElementById('username').value;
+  var regPassword = document.getElementById('password').value;
 
+  if (regUsername && regPassword) {
+    var users = JSON.parse(localStorage.getItem('users')) || [];
+    let userExist = users.some(user => user.username === regUsername && user.password === regPassword);
 
-  if (authenticateUser(username, password)) {
-      document.getElementById('login-container').style.display = 'none';
-      document.getElementById('secure-content').style.display = 'block';
-  } else {
+    if (userExist) {
+      alert('El usuario ha iniciado sesión exitosamente.');
+      console.log("Usuario autenticado correctamente");
+      window.location.href = "index.html";
+    } else {
       alert('Usuario o contraseña incorrectos.');
+    }
+  } else {
+    alert('Por favor, ingresa un nombre de usuario y una contraseña.');
   }
 });
-
-document.getElementById('logout-button').addEventListener('click', function() {
-  document.getElementById('secure-content').style.display = 'none';
-  document.getElementById('login-container').style.display = 'block';
-});
-
-function authenticateUser(username, password) {
-
-  let users = JSON.parse(localStorage.getItem('users')) || [];
-  return users.some(user => user.username === username && user.password === password);
-}
-
-
-window.onload = function() {
-  let users = JSON.parse(localStorage.getItem('users')) || [];
-  if (users.length > 0) {
-      document.getElementById('register-container').style.display = 'none';
-      document.getElementById('login-container').style.display = 'block';
-  }
-};
